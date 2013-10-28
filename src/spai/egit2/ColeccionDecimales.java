968beb07703783f64475a56893980720e22a3465
Raquel <Raquel@hp-ad4a5a826686>
2013-10-28 14:37:52 +0100

    issuesDev#3. Completado paulo gonzalez

--- src/spai/egit2/ColeccionDecimales.java
+++ src/spai/egit2/ColeccionDecimales.java
@@ -4,40 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ColeccionDecimales {
-    private List<Double> coleccion;
+	private List<Double> coleccion;
 
-    public ColeccionDecimales(double[] coleccion) {
-        this.coleccion = new ArrayList<Double>();
-        for (double item : coleccion) {
-            this.coleccion.add(item);
-        }
-    }
-
-    public ColeccionDecimales() {
-        this(new double[0]);
-    }
-
-    public Double suma() {
-        double suma = 0;
-        for (double item : this.coleccion) {
-            suma += item;
-        }
-        return suma;
-    }
-
-    public double mayor() {
-        double mayor = Double.NEGATIVE_INFINITY;
-        for (double item : this.coleccion) {
-            if (item > mayor)
-                mayor = item;
-        }
-        return mayor;
-    }
-
-    // Este metodo debe multiplicar la colección de decimales y devolver el valor.
-	public double multiplicar() {
-		// Implementar codigo
-		return -1;
+	public ColeccionDecimales(double[] coleccion) {
+		this.coleccion = new ArrayList<Double>();
+		for (double item : coleccion) {
+			this.coleccion.add(item);
+		}
 	}
 
+	public ColeccionDecimales() {
+		this(new double[0]);
+	}
+
+	public Double suma() {
+		double suma = 0;
+		for (double item : this.coleccion) {
+			suma += item;
+		}
+		return suma;
+	}
+
+	public double mayor() {
+		double mayor = Double.NEGATIVE_INFINITY;
+		for (double item : this.coleccion) {
+			if (item > mayor)
+				mayor = item;
+		}
+		return mayor;
+	}
+
+	// Este metodo debe multiplicar la colección de decimales y devolver el
+	// valor.
+	public double multiplicar() {
+		boolean primero = true;
+		double total = Double.NEGATIVE_INFINITY;
+		for (double item : this.coleccion) {
+			if (primero) {
+				total = item;
+				primero = false;
+				continue;
+			}
+			total = total * item;
+		}
+		return total;
+	}
 }
